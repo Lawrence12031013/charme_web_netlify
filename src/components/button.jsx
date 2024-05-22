dd7f083847ead6c9fbe9b8a12f896e87c05f96af
Chenyang Li <chenyangli@ChenyangMacBook-Air.local>
2024-05-22 10:35:12 +0800

    fix the large CSS RWD

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Button = () => {
   return (
     <div>
-        <div className='flex justify-evenly py-6 w-full fixed bottom-0 bg-white'>
+        <div className='flex justify-evenly py-6 w-full md:relative fixed bottom-0 bg-white'>
             <Link className='rounded border-2 border-black font-bold px-10 py-2' to="/login">Log in</Link>
             <Link className='rounded bg-black text-white font-bold px-10 py-2' to='/register'>Register</Link>
         </div>
